a0babcf2f258c8e2ec3cf342895254ee2bc36310
endercode99 <endercode99@gmail.com>
2020-12-22 17:30:37 +1100

    Added Test Cases

--- couchbase/src/test/java/DefaultTest.java
+++ couchbase/src/test/java/DefaultTest.java
@@ -1,17 +1,15 @@
-import com.couchbase.client.java.Cluster;
-import com.couchbase.client.java.Collection;
+import com.couchbase.client.java.*;
 import com.couchbase.client.java.json.JsonObject;
 import com.couchbase.client.java.kv.GetResult;
+import com.couchbase.client.java.query.QueryResult;
 import io.micronaut.context.ApplicationContext;
 
-import io.micronaut.context.env.PropertySource;
-import io.micronaut.core.util.CollectionUtils;
+import static com.couchbase.client.java.query.QueryOptions.queryOptions;
 
 import org.junit.Test;
 import util.TestUtil;
 
 import java.io.IOException;
-import java.util.Map;
 import java.util.Optional;
 
 import static org.junit.Assert.*;
@@ -46,6 +44,43 @@ public class DefaultTest {
 
         assertTrue(result.isPresent());
         assertEquals("bar", result.get().contentAs(JsonObject.class).getString("foo"));
+    }
 
+    @Test
+    public void basicKeyValueOperationsReactive() throws IOException, InterruptedException {
+        String bucketName = "default";
+        String imageName = "couchbase/server";
+
+        ApplicationContext applicationContext = TestUtil.initCouchbaseTestContainer(bucketName, imageName);
+        ReactiveCluster cluster = applicationContext.getBean(Cluster.class).reactive();
+
+        ReactiveCollection collection = cluster.bucket(bucketName).defaultCollection();
+
+        collection.upsert("id", JsonObject.create().put("foo", "bar")).block();
+        JsonObject document = collection.get("id").block().contentAsObject();
+
+        assertEquals("bar", document.getString("foo"));
+    }
+
+    @Test
+    public void basicN1QLOperations() throws IOException, InterruptedException {
+        String bucketName = "default";
+        String imageName = "couchbase/server";
+
+        ApplicationContext applicationContext = TestUtil.initCouchbaseTestContainer(bucketName, imageName);
+        Cluster cluster = applicationContext.getBean(Cluster.class);
+
+        Collection collection = cluster.bucket(bucketName).defaultCollection();
+
+        collection.upsert("id", JsonObject.create().put("foo", "bar"));
+
+        QueryResult result = cluster.query(
+                "select * from `default` where id = $id",
+                queryOptions().parameters(JsonObject.create().put("id", "foo"))
+        );
+
+        for (JsonObject jsonObject : result.rowsAs(JsonObject.class)) {
+            assertEquals("bar", jsonObject.getString("foo"));
+        }
     }
 }
